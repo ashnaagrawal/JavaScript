bd6b9ac17d2f459393a8fdfce752f1c0366c32ac
ashnaagrawal <51378366+ashnaagrawal@users.noreply.github.com>
2020-02-15 00:55:40 +0530

    Add files via upload

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,16 @@
+let age=23;
+let year=2020;
+
+console.log(age,year);
+
+age=20;
+year=2021;
+
+console.log(age,year);
+
+const points = 100;
+console.log(points);
+
+
+var score = 3444;
+console.log(score);

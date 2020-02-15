9ea6f1351a8eb9056726224a78a2f9edae06f920
ashnaagrawal <51378366+ashnaagrawal@users.noreply.github.com>
2020-02-16 00:46:33 +0530

    Add files via upload

---
+++ chapter_2/strings_sandbox.js
@@ -0,0 +1,32 @@
+//Strings
+console.log('hellooo, world!!!');
+
+let email = 'abc@gmail.com';
+console.log(email);
+
+//String Concatenation
+let FirstName ='Ashna';
+let LastName = 'Agrawal';
+console.log(FirstName+' ' +LastName);
+
+let FullName = FirstName+' ' +LastName;
+console.log(FullName);
+
+//getting characters
+console.log(FullName[0]);
+console.log(FirstName[3]);
+
+//string length
+console.log(FirstName.length);
+console.log(FullName.length);
+
+//string methods
+console.log(FirstName.toUpperCase());
+console.log(FullName.toUpperCase());
+
+let result =FullName.toLocaleLowerCase();
+console.log(result);
+
+let index = email.indexOf('@');
+console.log(index);
+

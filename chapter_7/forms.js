2dd1b1ef335da9dbbc479903bc3f813398e1bb2b
ashnaagrawal <51378366+ashnaagrawal@users.noreply.github.com>
2020-05-15 23:50:42 +0530

    Add files via upload

---
+++ chapter_7/forms.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('.signup-form');
+//const username = document.querySelector('#username')
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    //console.log(username.value);
+    console.log(form.username.value);
+
+
+});

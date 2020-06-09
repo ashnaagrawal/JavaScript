9ef8420333b03df79dbc4117294a9fb483e1b978
Ashna Agrawal <51378366+ashnaagrawal@users.noreply.github.com>
2020-06-09 23:35:02 +0530

    Add files via upload

---
+++ javaScript project/Weather app/scripts/forcast.js
@@ -0,0 +1,33 @@
+
+const key = 'gDucmta5kgMR2qouDUclFl94q39Vagj2';
+
+//get weather information
+const getWeather = async(id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query =`${id}?apikey=${key}`;
+
+    const response = await fetch(base+query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+
+// get city information
+const getCity = async(city) => {
+    const base ='http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+
+getCity('manchester').then(data => {
+        return getWeather(data.Key);
+    }).then(data => {
+        console.log(data);
+    }).catch(err => console.log(err));
+

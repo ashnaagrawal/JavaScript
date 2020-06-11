79d6bed84663d4f8693174e526852ea1bd08d0a8
Ashna Agrawal <51378366+ashnaagrawal@users.noreply.github.com>
2020-06-11 22:33:51 +0530

    Add files via upload

---
+++ chapter_14/deletingData.js
@@ -0,0 +1,18 @@
+// store data in local storage
+
+localStorage.setItem('name','mario');
+localStorage.setItem('age',50);
+
+
+// get data from local storage
+let name = localStorage.getItem('name');
+let age = localStorage.getItem('age');
+
+console.log(name,age);
+
+//deleting data from local storage
+//localStorage.removeItem('name');
+localStorage.clear();
+age=localStorage.getItem('age');
+name=localStorage.getItem('name');
+console.log(name,age);
